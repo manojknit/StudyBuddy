8375616693fb2db834a38cfad8540048f6c14cee
manojknit <erpatel@gmail.com>
2019-04-20 00:09:21 -0700

    config

--- studybuddyangularapp/src/app.global.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@angular/core';
-
-export const GlobalVariable = Object.freeze({
-    BASE_API_URL: 'http://localhost:4000/',
-    BASE_UI_URL: 'http://localhost:4200/'
-});
