91a13d13b4a8bea34e97ab49a6a2d77be7d0817b
Gyanesh Pandey <gyanesh.pandey@gmail.com>
2019-04-21 03:07:58 -0700

    Changes for Leaderboard

---
+++ studybuddyangularapp/src/app/Leaderboard.ts
@@ -0,0 +1,8 @@
+export default class Leaderboard {
+    user_id: String;
+    user_name: String;
+    started_on: Date;
+    course_id: String;
+    total_progress: Number;
+    max_video_date: Date;
+}
