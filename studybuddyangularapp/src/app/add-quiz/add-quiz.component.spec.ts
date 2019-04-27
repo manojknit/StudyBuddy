4c7a7c59803f4bdcb8f8165ddaffd5f9b73c5f4b
ShaliniMNarang <shalini.narang@sjsu.edu>
2019-04-27 09:22:36 -0700

    text case fixed

--- studybuddyangularapp/src/app/add-quiz/add-quiz.component.spec.ts
+++ studybuddyangularapp/src/app/add-quiz/add-quiz.component.spec.ts
@@ -1,25 +1,30 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { AddQuizComponent } from './add-quiz.component';
+import { AddQuizComponent } from './add-quiz.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-// describe('AddQuizComponent', () => {
-//   let component: AddQuizComponent;
-//   let fixture: ComponentFixture<AddQuizComponent>;
+describe('AddQuizComponent', () => {
+  let component: AddQuizComponent;
+  let fixture: ComponentFixture<AddQuizComponent>;
 
-//   beforeEach((() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AddQuizComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddQuizComponent ],
+      imports: [
+        RouterTestingModule, HttpClientTestingModule         
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AddQuizComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddQuizComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    // expect(component).toBeTruthy();
+  });
+});
